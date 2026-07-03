5096aaa94ef2461ce256b667002653a5d1f9f705
James <james@sglm.com>
2026-07-03 00:26:43 -0400

    fix: message deleted

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,4 @@
 <x-layouts.app title="{{ __('Inverse Logistics Service') }}">
-    <x-dispatcher::alert type="tip" class="">
-        {{ __('Welcome to Inverse Logistics Service Package for Laravel!') }}
-    </x-dispatcher::alert>
-
     <livewire:inverse-logistics::return-add-modal />
     <livewire:inverse-logistics::return-modal />
 
